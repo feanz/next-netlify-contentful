54123775fe7c8b3edd529d95a33e11d49f2d959e
richard.a.forrest@gmail.com <richard.a.forrest@gmail.com>
2022-04-26 12:10:16 +0700

    Change layout case

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/layout'
+import Layout from '../../components/Layout'
 import { fetchPostEntries } from '../../utils/postClient'
 
 export default function Post({ post }) {
